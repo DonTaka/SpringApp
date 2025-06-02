ba599ef07dac93ddefb32edb5b4f47ab19e468e8
j.riquelmee <j.riquelmee@profesor.duoc.cl>
2025-06-02 10:59:35 -0400

    Unidad 3.2 Implementacion de OpenAPI y Swagger-ui Integracion de librerias de documentacion y generacion de config base para proyecto FullStack I

--- src/main/java/com/example/SpringApp/Controller/UserController.java
+++ src/main/java/com/example/SpringApp/Controller/UserController.java
@@ -2,38 +2,45 @@ package com.example.SpringApp.Controller;
 
 import com.example.SpringApp.Model.User;
 import com.example.SpringApp.Services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-    @RestController
-    @RequestMapping("/users")
+@RestController
+@RequestMapping("/users")
+@Tag(name="Controlador Usuarios",description = "Servicio de gestion de usuarios Fullstack I")
 public class UserController {
 
     @Autowired
     UserService userService;
 
     @GetMapping
-    public String getUsers(){
+    @Operation(summary = "Obtener Usuarios",description = "Obtiene la lista de usuarios existentes en el sistema")
+    @ApiResponse(responseCode="200",description = "Consulta Exitosa")
+    public String getUsers() {
         return userService.getUsers();
     }
 
     @PostMapping
-    public String addUser(@RequestBody User user){
+    public String addUser(@RequestBody User user) {
         return userService.addUser(user);
     }
 
     @GetMapping("/{id}")
-    public String getUserById(@PathVariable int id){
+    public String getUserById(@PathVariable int id) {
         return userService.getuser(id);
     }
 
     @DeleteMapping("/{id}")
-    public String deleteUserById(@PathVariable int id){
+    public String deleteUserById(@PathVariable int id) {
         return userService.deleteUser(id);
     }
 
     @PutMapping("/{id}")
-    public String putUserById(@PathVariable int id, @RequestBody User user){
-        return userService.updateUser(id,user);
+    public String putUserById(@PathVariable int id, @RequestBody User user) {
+        return userService.updateUser(id, user);
     }
 }
